99d32ec831f9bde8437834973a039c8d7c8d903f
Matt Knighten <jason.knighten@revature.com>
2020-05-29 16:34:10 -0400

    added checkStartDate function

--- TRMSProjectPattonZ/src/main/webapp/trms.js
+++ TRMSProjectPattonZ/src/main/webapp/trms.js
@@ -1,6 +1,8 @@
 window.onload = function() {
     console.log("in onload");
+    document.getElementById("trmsFormSub").disabled = true;
     document.getElementById("trmsFormSub").addEventListener("click", createFormRequest, false);
+    document.getElementById("startDate").addEventListener("change", checkStartDate, false);
 }
 
 function jsonBuilder(){
@@ -28,4 +30,20 @@ function createFormRequest(){
     xhr.open("POST", `http://localhost:8080/TRMSProjectPattonZ/TRMSForm`, true);
     var payload = jsonBuilder();
     xhr.send(payload);
+}
+
+function checkStartDate(start){
+    let now=Date.now();//give milliseconds of current time
+    start=Date.parse(document.getElementById("startDate").value); //give milliseconds of start date
+    console.log(now+"  verus " + start);
+    if(start>now+604800000){ 
+        document.getElementById("tooShort").innerHTML="";
+        console.log("true");
+        document.getElementById("trmsFormSub").disabled = false;
+    }else{
+        document.getElementById("tooShort").innerHTML="<h3>The start date much be at least 7 days after the current date</h3>";
+        console.log("false");
+        document.getElementById("trmsFormSub").disabled = true;
+
+    }
 }
